1b84b77c9f18c95107dade1993d32a1ea32879fb
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-10 20:56:33 +0300

    feat: add Animation on scrolling

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-import { clickLabel } from "./utils.js";
+import { animateOnScroll, clickLabel } from "./utils.js";
 // select Labels
 const labelElements = document.querySelectorAll("label");
 
 labelElements.forEach((element) => clickLabel(element));
+// animate on scroll
+const sectionElements = document.querySelectorAll("section");
+
+animateOnScroll(sectionElements);
--- utils.js
+++ utils.js
@@ -6,3 +6,22 @@ export function clickLabel(element) {
     }
   });
 }
+
+// animate on scroll
+export function animateOnScroll(sectionElements) {
+  let options = { threshold: 0.5 };
+
+  let callbackfn = (entries) => {
+    entries.forEach((entry) => {
+      //desired code
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+      } else {
+        entry.target.classList.remove("in-view");
+      }
+    });
+  };
+  const observer = new IntersectionObserver(callbackfn, options);
+
+  sectionElements.forEach((element) => observer.observe(element));
+}
